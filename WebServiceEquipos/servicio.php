8f62cc1c606adf0641c34b8aeae10e33b78e17f0
cristhiandres <cris.adc94@gmail.com>
2016-02-08 15:13:51 -0500

    s
    s

--- WebServiceEquipos/servicio.php
+++ WebServiceEquipos/servicio.php
@@ -7,9 +7,9 @@ $server = new soap_server();
 $ns = "urn:paiswsl";
 $server->configureWSDL("Consulta Pais",$ns);
 //$server->schemaTargetNamespace = $ns;
-$server->soap_defencoding = 'UTF-8';
-$server->decode_utf8 = false;
-$server->encode_utf8 = true;
+//$server->soap_defencoding = 'UTF-8';
+//$server->decode_utf8 = false;
+//$server->encode_utf8 = true;
 
 /*
 $server->register("Listar",
